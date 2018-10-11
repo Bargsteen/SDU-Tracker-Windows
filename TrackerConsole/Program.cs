06070dc0cd1b1b44367cef5bad56ea17ec459190
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2018-10-11 13:31:54 +0200

    Create classlib. Create console proj (for unix testing)

---
+++ TrackerConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TrackerLib;
+
+namespace TrackerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(TrackerLib.Class1.HelloWorld());
+        }
+    }
+}
---
+++ TrackerLib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TrackerLib
+{
+    public class Class1
+    {
+        public static string HelloWorld() {
+            return "Hello World From Lib";
+        }
+    }
+}
